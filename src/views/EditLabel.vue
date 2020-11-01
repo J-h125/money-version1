7bde74bdd814adf603f1f3257eee01efc7a2e5a2
JH <473731589@qq.com>
2020-11-01 11:35:07 +0800

    将tag的string改为id和name

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        编辑标签
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'EditLable'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
