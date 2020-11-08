4d9b079cf46935792daf198a5d0522932ef4aa6f
JH <473731589@qq.com>
2020-11-08 15:27:36 +0800

    封裝record的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,16 +18,8 @@
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
     import Tags from '@/components/Money/Tags.vue';
-    import {Component, Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
+    import {Component} from 'vue-property-decorator';
     import FormItem from '@/components/Money/FormItem.vue';
-
-
-
-
-    const recordList =recordListModel.fetch();
-
-
 
 
     @Component({
@@ -35,7 +27,7 @@
     })
     export default  class Money extends Vue{
         tags= window.tagList;
-        recordList:RecordItem[] = recordList;
+        recordList = window.recordList;
         record:RecordItem = {tags:[],notes:'',type:'-',amount:0}
 
         onUpdateTags(value:string[]){
@@ -46,13 +38,9 @@
         }
 
         saveRecord(){
-            recordListModel.create(this.record);
+            window.createRecord(this.record);
+        }
 
-        }
-        @Watch('recordList')
-        onRecordListChange(){
-          recordListModel.save()
-        }
     };
 </script>
 <style lang="scss">
