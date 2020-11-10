1809d0a93dd8a4c6c48bf468000344b65db6362d
JH <473731589@qq.com>
2020-11-10 15:12:44 +0800

    tagStore 使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -20,14 +20,16 @@
     @Component({
         computed:{
             tagList(){
-                //tagList = store.fetchTags();
-                return []
+                return this.$store.state.tagList;
             }
         }
     })
     export default class Tags extends Vue{
 
        selectedTags:string[] = [];
+       created(){
+           this.$store.commit('fetchTags');
+       }
 
        toggle(tag:string){
           const index =  this.selectedTags.indexOf(tag);
@@ -40,10 +42,9 @@
        }
         create(){
            const name = window.prompt('请输入标签名');
-           if(!name){
-              return   window.alert('标签名不能为空');
-           }
-           // store.createTag(name);
+           if(!name){return   window.alert('标签名不能为空');}
+           this.$store.commit('createTag',name);
+
 
         }
     };
